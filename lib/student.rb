5734cae24ca19c577441d9cfcf09548471c369fc
Michael Roose <mjroose@gmail.com>
2018-09-26 00:57:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,4 +44,11 @@ class Student
     student.save
     student
   end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(name, grade, id)
+  end
 end
